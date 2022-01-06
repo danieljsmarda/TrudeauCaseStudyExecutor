5458485cb6c6f3ebde8e99aa04bf1a8c89b36812
Daniel Smarda <danieljsmarda@gmail.com>
2022-01-06 10:23:47 +0100

    Add files via upload

---
+++ execute.bat
@@ -0,0 +1,3 @@
+"<path-to-python-environment-file>" "<path-to-updater.py>"
+:: Example: "C:/Users/danie/Miniconda3/envs/holidaycheckinterview/python.exe" "c:/Users/danie/Documents/Jobs/Zurich/HolidayCheck/CaseStudy/HolidayCheckInterview/updater.py"
+pause
